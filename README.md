fef6c6d8c942d92e861f91054925dda221d3dabc
Aarni Koskela <akx@iki.fi>
2017-02-10 20:22:57 +0200

    Add parameter parsing
    I'm slightly drunk.

--- README.md
+++ README.md
@@ -14,3 +14,18 @@ this tool comes in.
 $ node --trace-sync-io slow_script.js 2>trace.txt
 $ node summarize-sync-io.js < trace.txt
 ```
+
+## Command line parameters
+
+### Excluded stacks
+
+By default, writes, `require`s and `close`s are filtered out.
+
+To not filter them, use the `--writes`/`--module-loads`/`--closes` parameters, respectively.
+
+You can also use `--all` to exclude all of those filters from being used.
+
+### Module matching
+
+Add `--match=my-module` to filter the result to stacks that have at least one entry
+from a module whose path contains `my-module`.
--- summarize-sync-io.js
+++ summarize-sync-io.js
@@ -1,15 +1,13 @@
-const excludes = require('./lib/excludes');
+const filter = require('./lib/filter');
 const parse = require('./lib/parse');
 const printHistogram = require('./lib/print-histogram');
+const parseOptions = require('./lib/parse-options');
 
 function count(map, key, value = 1) {
   map[key] = (map[key] || 0) + value;
 }
 
-function summarize(entries) {
-  Object.entries(excludes).forEach(([name, fn]) => {
-    entries = fn(entries);
-  });
+function summarize(options, entries) {
   const callsByImmediateCaller = {};
   entries.forEach((stack) => {
     const syncFn = stack[0].fn;
@@ -21,4 +19,19 @@ function summarize(entries) {
   printHistogram(callsByImmediateCaller);
 }
 
-parse(process.stdin).then(summarize);
+const defaults = {
+  moduleLoads: false,
+  closes: false,
+  writes: false,
+  match: null,
+};
+
+const options = parseOptions(defaults, process.argv);
+if (options.all) {
+  Object.keys(options).forEach((key) => {
+    if (options[key] === false) options[key] = true;
+  })
+}
+parse(process.stdin)
+  .then((entries) => filter(options, entries))
+  .then((entries) => summarize(options, entries));
